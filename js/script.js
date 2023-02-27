cc06b244ccabbb5ee479673cc71e1bfd755a1f8e
0x45R <robertplawski8@gmail.com>
2023-02-28 00:22:22 +0100

    Little code refactor

--- js/script.js
+++ js/script.js
@@ -8,24 +8,24 @@ async function getQuote(){
   const response = await fetch("https://api.quotable.io/random", { headers: {"Content-Type":"application/json"}})
   return response.json()
 }
+
+function assignData(data){
+  quoteText.innerText = data.content
+  quoteAuthor.innerText = data.author
+  refreshButton.classList.remove("loading")
+}
+
 getQuote().then((data)=>{
     main.appendChild(quoteText)
     quoteText.classList.add('quote-text')
 
     main.appendChild(quoteAuthor)
     quoteAuthor.classList.add("quote-author")
-
-    quoteText.innerText = data.content
-    quoteAuthor.innerText = data.author
-    refreshButton.classList.remove("loading")
+    
+    assignData(data);
   }
 )
 refreshButton.addEventListener("click", ()=>{
   refreshButton.classList.add("loading")
-  getQuote().then((data)=>{
-      quoteText.innerText = data.content
-      quoteAuthor.innerText = data.author
-      refreshButton.classList.remove("loading")
-    }
-  )
+  getQuote().then(assignData)
 })
