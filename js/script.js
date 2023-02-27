0a832301cf1c0f479209a69a0a3dbd516f5ec1ec
0x45R <robertplawski8@gmail.com>
2023-02-27 23:47:18 +0100

    Changed website look

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 const refreshButton = document.querySelector(".generate-new-quote")
-const quoteText = document.querySelector('.quote-text')
-const quoteAuthor = document.querySelector(".quote-author")
+const main = document.querySelector(".quote-container")
+const quoteText = document.createElement("p")
+const quoteAuthor = document.createElement("p")
+
 
 async function getQuote(){
   const response = await fetch("https://api.quotable.io/random", { headers: {"Content-Type":"application/json"}})
   return response.json()
 }
+getQuote().then((data)=>{
+    main.appendChild(quoteText)
+    quoteText.classList.add('quote-text')
 
+    main.appendChild(quoteAuthor)
+    quoteAuthor.classList.add("quote-author")
+    
+    refreshButton.classList.remove("loading")
+
+    quoteText.innerText = data.content
+    quoteAuthor.innerText = data.author
+  }
+)
 refreshButton.addEventListener("click", ()=>{
   refreshButton.classList.add("loading")
   getQuote().then((data)=>{
